b4b1e590d7355e1ca6ad1765b4c418234a011862
Harsh Sachin Chandekar <59416466+harshchan@users.noreply.github.com>
2021-04-22 11:34:37 +0530

    with all functions

--- trees/BST.cpp
+++ trees/BST.cpp
@@ -68,6 +68,28 @@ treeNode* findmin(treeNode *root)
   return current;
 }
 
+void search(treeNode * roo,int data)
+{int level=0;
+	if(roo==NULL)
+	{
+		cout<<"\n tree is empty";
+	}
+	else
+	{
+	if(data>roo->data)
+	{
+	level++;	search(roo->right,data);
+	}
+	if(data<roo->data)
+	{
+	level++;	search(roo->left,data);
+	}
+	if(data==roo->data)
+	{
+		cout<<"\n data is present in tree at  "<<level<<" level";
+	}
+	}
+}
 treeNode* Delete(treeNode *root, int key)
  {
    //base case
@@ -82,45 +104,57 @@ treeNode* Delete(treeNode *root, int key)
    {
      root->right=Delete(root->right, key);
    }
-
+//key found 
    else
-   {
+   {//node is leaf node
+   //no children
      if(root->left==NULL && root->right==NULL)
      {
        delete root;
        root=NULL;
      }
-
+//node has right child but no left child
      else if(root->left==NULL)
      {
        treeNode *temp=root;
        root=root->right;
        delete temp;
      }
-
+//node has left child but no right child
      else if(root->right==NULL)
      {
        treeNode *temp=root;
        root=root->left;
        delete temp;
      }
+     //node has both the childs 
      else
      {
-       treeNode *temp=findmin(root->right);
+       treeNode *temp=findmin(root->right);//taking minimum value for right subtree of node to be deleted
        root->data=temp->data;
        root->right=Delete(root->right, temp->data);
      }
    }
    return root;
  }
+ 
+ int  commonAncestor(int d1,int d2,treeNode *roo)
+ {
+ 	if(roo->data>d1 && roo->data>d2)
+ 	commonAncestor(d1,d2,roo->left);
+ 	else if(roo->data<d1 && roo->data<d2)
+ 	commonAncestor(d1,d2,roo->right);
+ 	else
+ 	return roo->data;
+ }
 
 int main()
 {
-  treeNode *root=NULL;
-  int x,n;
+  treeNode *root=NULL,*temp;
+  int x,n,r,d1,d2;
   do{
   
-  cout<<"\n 1.Create tree \n 2.Inorder \n 3.Preorder \n 4.Postorder \n 5. Delete \n ";
+  cout<<"\n 1.Create tree \n 2.Inorder \n 3.Preorder \n 4.Postorder \n 5. Delete \n6.search \n7.Find Common Ancestor";
   cin>>x;
   switch(x)
   {
@@ -143,12 +177,23 @@ int main()
 		{
 			cout<<"\n Enter Data to be deleted";
 			cin>>n;
-			root=Delete(root,n);
+			root=Delete(root,n);break;
 		}
+	case 6:
+			cout<<"\n Enter data to be searched";
+			cin>>r;
+			search(root,r);break;
+			
+	case 7:
+		cout<<"\n Enter 2 data to be searched";
+		cin>>d1>>d2;
+		
+		cout<<"\n The common ancestor is"<<commonAncestor(d1,d2,root);break;
+			
 		
   }
 
-}while(x<6 && x>0);
+}while(x<8 && x>0);
 
   return 0;
 }
